3b1091764bee0c7c365d5b220eb1e6ce87286ef5
youngchul,yu <125463984+siadaddy@users.noreply.github.com>
2025-07-03 22:16:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ pio.templates.default = "custom_font"
 # ✅ 캐시 기반 데이터 로딩 함수
 @st.cache_data(show_spinner="📦 데이터 로딩 중... 조금만 기다려주세요!")
 def load_data():
-    DB_PATH = "instacart_min_v1_1000.duckdb"  # ← 변경된 경량 버전 경로
+    DB_PATH = "instacart_min_v2_1000.duckdb"  # ← 변경된 경량 버전 경로
     con = duckdb.connect(DB_PATH)
 
     queries = {
